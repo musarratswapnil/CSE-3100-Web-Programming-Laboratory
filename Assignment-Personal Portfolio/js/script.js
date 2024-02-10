6976b3e4ed01086d8f62726a7d5685dac69fd7e9
Swapnil <shaeer.musarrat@gmail.com>
2024-02-11 00:18:22 +0600

    lab2 modified and partially completed using html css js

--- Assignment-Personal Portfolio/js/script.js
+++ Assignment-Personal Portfolio/js/script.js
@@ -1,4 +1,5 @@
 //toggle icon bar
+
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -17,7 +18,7 @@ let navLinks=document.querySelectorAll('header nav a');
 window.onscroll = () =>{
     sections.forEach(sec => {
         let top= window.scrollY;
-        l et offset= sec.offsetTop - 100;
+        let offset= sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
@@ -27,7 +28,17 @@ window.onscroll = () =>{
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+
+            // active sections for animation onscroll
+
+            sec.classList.add('show-animate');
         }
+
+        // if want to use animation that repeats on scroll use this
+        else{
+            sec.classList.remove('show-animate');
+        }
+
     });
 
     //sticky header
@@ -38,4 +49,9 @@ window.onscroll = () =>{
 
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer=document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight); 
 }
